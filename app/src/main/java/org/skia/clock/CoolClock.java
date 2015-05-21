3b19a5b02b66e857e538ebe0704be8487f21cbd5
Hal Canary <halcanary@google.com>
2015-05-20 21:45:28 -0400

    analog clock

--- app/src/main/java/org/skia/clock/CoolClock.java
+++ app/src/main/java/org/skia/clock/CoolClock.java
@@ -2,6 +2,7 @@ package org.skia.clock;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 import java.util.Calendar;
@@ -16,7 +17,47 @@ public class CoolClock extends View {
     private Paint fPaint;
     private Date fDate;
     private Calendar fCalendar;
+    private Path fSecondPath;
+    private Path fHourPath;
+    private Path fMinutePath;
+    private Path fMarks;
+    private Paint fStroke;
 
+    private int fScale;
+
+    private void setScale(int scale) {
+        if (scale == fScale) {
+            return;
+        }
+        fScale = scale;
+        fSecondPath = new Path();
+        fSecondPath.lineTo(0.0f, scale * -0.45f);
+
+        fHourPath = new Path();
+        fHourPath.lineTo(0.0f, scale * -0.25f);
+
+        fMinutePath = new Path();
+        fMinutePath.lineTo(0.0f, scale * -0.40f);
+
+        fMarks = new Path();
+        float r1 = scale * 0.45f;
+        float r2 = scale * 0.5f;
+        float r3 = scale * 0.47f;
+        for (int i = 0; i < 12; ++i) {
+            double angleRad = 0.52359877559829887307 * i;
+            double c = Math.cos(angleRad);
+            double s = Math.sin(angleRad);
+            fMarks.moveTo((float) (r1 * c), (float) (r1 * s));
+            fMarks.lineTo((float) (r2 * c), (float) (r2 * s));
+            for (int j = 0; j < 5; ++j) {
+                angleRad += .10471975511965977461;
+                c = Math.cos(angleRad);
+                s = Math.sin(angleRad);
+                fMarks.moveTo((float)(r1 * c), (float)(r1 * s));
+                fMarks.lineTo((float)(r3 * c), (float)(r3 * s));
+            }
+        }
+    }
     public CoolClock(Context ctx) {
         super(ctx);
         fPaint = new Paint();
@@ -24,33 +65,71 @@ public class CoolClock extends View {
         fPaint.setTextSize(120);
         fDate = new Date();
         fCalendar = Calendar.getInstance();
+
+        fStroke = new Paint();
+        fStroke.setAntiAlias(true);
+        fStroke.setStyle(Paint.Style.STROKE);
+        fStroke.setStrokeWidth(10.0f);
+        fStroke.setStrokeCap(Paint.Cap.ROUND);
     }
 
     protected void onDraw(android.graphics.Canvas canvas) {
         fDate.setTime(System.currentTimeMillis());
         fCalendar.setTime(fDate);
 
-        // // You may want these values.
-        // int day_of_month = fCalendar.get(Calendar.DAY_OF_MONTH);
-        // int month = 1 + fCalendar.get(Calendar.MONTH);
-        // int year = fCalendar.get(Calendar.YEAR);
-        // int millisecond = fCalendar.get(Calendar.MILLISECOND);
+        int day = fCalendar.get(Calendar.DAY_OF_MONTH);
+        int month = 1 + fCalendar.get(Calendar.MONTH);
+        int year = fCalendar.get(Calendar.YEAR);
+        int millisecond = fCalendar.get(Calendar.MILLISECOND);
 
         int hour = fCalendar.get(Calendar.HOUR);
         int minute = fCalendar.get(Calendar.MINUTE);
         int second = fCalendar.get(Calendar.SECOND);
-        boolean is_pm = Calendar.PM == fCalendar.get(Calendar.AM_PM);
+        //boolean is_pm = Calendar.PM == fCalendar.get(Calendar.AM_PM);
 
         // ====================================
-        // REPLACE THIS CODE WITH A COOL CLOCK!
-        String date = String.format("%2d:%02d:%02d %s",
-                hour, minute, second, is_pm ? "p.m." : "a.m.");
+        int height = canvas.getHeight();
+        int width = canvas.getWidth();
+        int scale = height < width ? height : width;
+        this.setScale(scale);
+        canvas.save();
+        canvas.translate(0.5f * width, 0.5f * height);
         canvas.drawRGB(255, 255, 128);
+        fPaint.setColor(0xFFDDDDFF);
+        canvas.drawCircle(0.0f, 0.0f, scale * 0.45f, fPaint);
+        fPaint.setColor(0xFF000000);
+
+        canvas.save();
+        canvas.rotate(hour * 30f + minute * 0.5f);
+        canvas.scale(-1.0f, 1.0f);
+        fStroke.setStrokeWidth(20.0f);
+        canvas.drawPath(fHourPath, fStroke);
+        canvas.restore();
+
+        canvas.save();
+        canvas.rotate(minute * 6.0f + second * 0.1f);
+        canvas.scale(-1.0f, 1.0f);
+        fStroke.setStrokeWidth(15.0f);
+        canvas.drawPath(fMinutePath, fStroke);
+        fStroke.setStrokeWidth(10.0f);
+        canvas.restore();
+
+
+        canvas.save();
+        canvas.rotate(second * 6.0f + millisecond * 0.006f);
+        canvas.scale(-1.0f, 1.0f);
+        canvas.drawPath(fSecondPath, fStroke);
+        canvas.restore();
+
+        canvas.drawPath(fMarks, fStroke);
+
+        canvas.restore();
+        String date = String.format("%4d-%02d-%02d",
+                year, month, day);
         canvas.drawText(date, 10, 130, fPaint);
-        // REPLACE THIS CODE WITH A COOL CLOCK!
         // ====================================
 
-        long DELAY_MILLISECONDS = 300;
+        long DELAY_MILLISECONDS = 100;
         this.postInvalidateDelayed(DELAY_MILLISECONDS);
     }
 
